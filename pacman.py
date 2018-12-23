97a80de3aa5aa6e8f42d19e01a458d689934060f
owner <shenxu183@yahoo.com>
2018-12-23 16:06:26 -0400

    Create a border
    The changes are made in order to specify where the location where game is taking place.
    
    To do so, a Turtle object was created and set in location (-300,-300). It then draws a long line before turning counterclockwise, and continues to do so for a total of 4 times. The effect is that a white boundary was created, where all gameplay will occur within.
    
    A limitation of this build is that Pac-Man still needs to be created

--- pacman.py
+++ pacman.py
@@ -5,6 +5,21 @@ import random
 
 # create a blank screen that serves as background
 game=turtle.Screen()
-game.bgcolor("blue")
+game.bgcolor("black")
 game.title("Pacman")
 
+boundary=turtle.Turtle()
+boundary.speed(0)
+boundary.color("white")
+boundary.penup()
+boundary.setposition(-300,-300)
+boundary.pendown()
+boundary.pensize(3)
+for eachline in range(4):
+    boundary.fd(600)
+    boundary.lt(90)
+# hides the triangle used to draw boundaries
+boundary.hideturtle()
+
+
+turtle.mainloop()
